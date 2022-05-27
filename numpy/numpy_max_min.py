94cbd6013de73375015578d9e662ff70503ef68c
Gopal Sharma <92174457+GOPAL181@users.noreply.github.com>
2022-05-27 21:20:08 +0530

    Create numpy_max_min.py
    WAP which  take array from user and print maximum and minimum value

---
+++ numpy/numpy_max_min.py
@@ -0,0 +1,6 @@
+#finding the max and min value of numpy array
+import numpy as np
+b=np.array([1,2,3,4,5])
+maximum=b.max()
+minimum=b.min()
+print("the max value and min value of this array is ",maximum,minimum)
